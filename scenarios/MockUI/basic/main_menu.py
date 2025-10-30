678744a89550cad3d6529a9e755e3218f80493ce
maggo83 <marco.kruse@gmx.de>
2025-10-30 12:37:38 +0100

    Added Option to directly load Seed from (detected) SmartCard on MainMenu

--- scenarios/MockUI/basic/main_menu.py
+++ scenarios/MockUI/basic/main_menu.py
@@ -10,7 +10,11 @@ def MainMenu(parent, *args, **kwargs):
 
     #add "process inputs" label if any relevant input is available
     #relevant input possibilities are QR Scanner, SD Card, or (to sign messages) a registered wallet
-    if (state and ((state.hasQR and state.enabledQR) or (state.hasSD and state.enabledSD and state.detectedSD) or (state.active_wallet and not state.active_wallet.isMultiSig))):
+    if (state and ((state.hasQR and state.enabledQR) 
+                   or (state.hasSD and state.enabledSD and state.detectedSD) 
+                   or (state.active_wallet and not state.active_wallet.isMultiSig)
+                   or (state.active_wallet is None and state.hasSmartCard and state.enabledSmartCard and state.detectedSmartCard)
+                   )):
         menu_items.append(("Process input", None))
         if (state.hasQR and state.enabledQR):
             menu_items.append(("Scan QR", "scan_qr"))
@@ -18,6 +22,8 @@ def MainMenu(parent, *args, **kwargs):
             menu_items.append(("Load File from SD", "load_sd"))
         if (state and state.active_wallet and not state.active_wallet.isMultiSig):
             menu_items.append(("Sign Message", "sign_message"))
+        if (state and state.active_wallet is None and state.hasSmartCard and state.enabledSmartCard and state.detectedSmartCard):
+            menu_items.append(("Load SeedPhrase From SmartCard", "import_from_smartcard"))
 
     menu_items.append(("Manage Settings", None))
     if (state and not state.active_wallet is None):
