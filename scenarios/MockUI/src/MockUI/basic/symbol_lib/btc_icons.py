11f992b033a120e176e6b66596ba1e226581d1a2
maggo83 <marco.kruse@gmx.de>
2026-04-23 15:38:13 +0200

    Imported v0.1.11 of bitcoinicons
    includes amongst others:
    -battery
    -power
    -smartcard

--- scenarios/MockUI/src/MockUI/basic/symbol_lib/btc_icons.py
+++ scenarios/MockUI/src/MockUI/basic/symbol_lib/btc_icons.py
@@ -1,4 +1,4 @@
-"""Bitcoin icon library aggregator — 126 icons at 42×42 px.
+"""Bitcoin icon library aggregator — 165 icons at 42×42 px.
 
 AUTO-GENERATED — do not edit directly.
 Regenerate with:
@@ -18,22 +18,34 @@ from .icons.arrow_down import ARROW_DOWN
 from .icons.arrow_left import ARROW_LEFT
 from .icons.arrow_right import ARROW_RIGHT
 from .icons.arrow_up import ARROW_UP
+from .icons.bank import BANK
+from .icons.battery_2 import BATTERY_2
+from .icons.battery_3 import BATTERY_3
+from .icons.battery_4 import BATTERY_4
+from .icons.battery_empty import BATTERY_EMPTY
+from .icons.battery_full import BATTERY_FULL
 from .icons.bell import BELL
+from .icons.bitaxe import BITAXE
 from .icons.bitcoin import BITCOIN
 from .icons.bitcoin_circle import BITCOIN_CIRCLE
+from .icons.bitty import BITTY
 from .icons.block import BLOCK
+from .icons.boat import BOAT
 from .icons.brush import BRUSH
 from .icons.buoy import BUOY
 from .icons.calendar import CALENDAR
+from .icons.camera import CAMERA
 from .icons.car import CAR
 from .icons.caret_down import CARET_DOWN
 from .icons.caret_left import CARET_LEFT
 from .icons.caret_right import CARET_RIGHT
 from .icons.caret_up import CARET_UP
 from .icons.cart import CART
+from .icons.chair import CHAIR
 from .icons.channel import CHANNEL
 from .icons.channels import CHANNELS
 from .icons.check import CHECK
+from .icons.citadel import CITADEL
 from .icons.clear_character import CLEAR_CHARACTER
 from .icons.clock import CLOCK
 from .icons.cloud import CLOUD
@@ -52,24 +64,35 @@ from .icons.copy import COPY
 from .icons.credit_card import CREDIT_CARD
 from .icons.cross import CROSS
 from .icons.devices import DEVICES
+from .icons.dice import DICE
+from .icons.drops import DROPS
 from .icons.edit import EDIT
 from .icons.ellipsis import ELLIPSIS
+from .icons.email import EMAIL
+from .icons.entropy import ENTROPY
 from .icons.exchange import EXCHANGE
 from .icons.exit import EXIT
 from .icons.export import EXPORT
+from .icons.faucet import FAUCET
 from .icons.file import FILE
+from .icons.fingerprint import FINGERPRINT
 from .icons.flip_horizontal import FLIP_HORIZONTAL
 from .icons.flip_vertical import FLIP_VERTICAL
 from .icons.gear import GEAR
+from .icons.gift import GIFT
 from .icons.globe import GLOBE
 from .icons.graph import GRAPH
 from .icons.grid import GRID
+from .icons.hashes import HASHES
+from .icons.hat_and_glasses import HAT_AND_GLASSES
 from .icons.hidden import HIDDEN
 from .icons.home import HOME
 from .icons.info import INFO
 from .icons.info_circle import INFO_CIRCLE
 from .icons.invoice import INVOICE
+from .icons.iou import IOU
 from .icons.key import KEY
+from .icons.keyboard import KEYBOARD
 from .icons.lightning import LIGHTNING
 from .icons.lightning_circle import LIGHTNING_CIRCLE
 from .icons.link import LINK
@@ -82,10 +105,12 @@ from .icons.milk import MILK
 from .icons.miner import MINER
 from .icons.mining import MINING
 from .icons.mining_device import MINING_DEVICE
+from .icons.mining_pool import MINING_POOL
 from .icons.minus import MINUS
 from .icons.mixed import MIXED
 from .icons.mnemonic import MNEMONIC
 from .icons.moon import MOON
+from .icons.nfc import NFC
 from .icons.no_dollars import NO_DOLLARS
 from .icons.node import NODE
 from .icons.node_0_connections import NODE_0_CONNECTIONS
@@ -94,17 +119,22 @@ from .icons.node_2_connections import NODE_2_CONNECTIONS
 from .icons.node_3_connections import NODE_3_CONNECTIONS
 from .icons.node_hardware import NODE_HARDWARE
 from .icons.pantheon import PANTHEON
+from .icons.paper_bitcoin import PAPER_BITCOIN
 from .icons.password import PASSWORD
 from .icons.photo import PHOTO
 from .icons.pie_chart import PIE_CHART
 from .icons.plus import PLUS
 from .icons.podcast import PODCAST
 from .icons.point_of_sale import POINT_OF_SALE
+from .icons.power import POWER
+from .icons.printer import PRINTER
 from .icons.proxy import PROXY
 from .icons.qr_code import QR_CODE
 from .icons.question import QUESTION
 from .icons.question_circle import QUESTION_CIRCLE
+from .icons.receipt import RECEIPT
 from .icons.receive import RECEIVE
+from .icons.receive_limit import RECEIVE_LIMIT
 from .icons.refresh import REFRESH
 from .icons.relay import RELAY
 from .icons.rocket import ROCKET
@@ -116,13 +146,16 @@ from .icons.scan import SCAN
 from .icons.sd_card import SD_CARD
 from .icons.search import SEARCH
 from .icons.send import SEND
+from .icons.send_limit import SEND_LIMIT
 from .icons.share import SHARE
 from .icons.shared_wallet import SHARED_WALLET
 from .icons.shield import SHIELD
 from .icons.sign import SIGN
+from .icons.siren import SIREN
 from .icons.smartcard import SMARTCARD
 from .icons.snowflake import SNOWFLAKE
 from .icons.sofa import SOFA
+from .icons.split import SPLIT
 from .icons.star import STAR
 from .icons.sun import SUN
 from .icons.tag import TAG
@@ -130,18 +163,24 @@ from .icons.tip_jar import TIP_JAR
 from .icons.transactions import TRANSACTIONS
 from .icons.transfer import TRANSFER
 from .icons.trash import TRASH
+from .icons.tree_structure import TREE_STRUCTURE
 from .icons.two_keys import TWO_KEYS
 from .icons.unlock import UNLOCK
 from .icons.unmixed import UNMIXED
 from .icons.usb import USB
 from .icons.verify import VERIFY
 from .icons.visible import VISIBLE
+from .icons.volume_1 import VOLUME_1
+from .icons.volume_2 import VOLUME_2
+from .icons.volume_max import VOLUME_MAX
+from .icons.volume_mute import VOLUME_MUTE
 from .icons.wallet import WALLET
+from .icons.water_bucket import WATER_BUCKET
 
 
 class BTC_ICONS:
     """
-    Library of Bitcoin-themed icons (126 total, 42×42 px).
+    Library of Bitcoin-themed icons (165 total, 42×42 px).
 
     Icons default to white; pass a color to tint them:
         BTC_ICONS.WALLET(lv.color_hex(0xFF0000))  # red
@@ -155,22 +194,34 @@ class BTC_ICONS:
     ARROW_LEFT = ARROW_LEFT
     ARROW_RIGHT = ARROW_RIGHT
     ARROW_UP = ARROW_UP
+    BANK = BANK
+    BATTERY_2 = BATTERY_2
+    BATTERY_3 = BATTERY_3
+    BATTERY_4 = BATTERY_4
+    BATTERY_EMPTY = BATTERY_EMPTY
+    BATTERY_FULL = BATTERY_FULL
     BELL = BELL
+    BITAXE = BITAXE
     BITCOIN = BITCOIN
     BITCOIN_CIRCLE = BITCOIN_CIRCLE
+    BITTY = BITTY
     BLOCK = BLOCK
+    BOAT = BOAT
     BRUSH = BRUSH
     BUOY = BUOY
     CALENDAR = CALENDAR
+    CAMERA = CAMERA
     CAR = CAR
     CARET_DOWN = CARET_DOWN
     CARET_LEFT = CARET_LEFT
     CARET_RIGHT = CARET_RIGHT
     CARET_UP = CARET_UP
     CART = CART
+    CHAIR = CHAIR
     CHANNEL = CHANNEL
     CHANNELS = CHANNELS
     CHECK = CHECK
+    CITADEL = CITADEL
     CLEAR_CHARACTER = CLEAR_CHARACTER
     CLOCK = CLOCK
     CLOUD = CLOUD
@@ -189,24 +240,35 @@ class BTC_ICONS:
     CREDIT_CARD = CREDIT_CARD
     CROSS = CROSS
     DEVICES = DEVICES
+    DICE = DICE
+    DROPS = DROPS
     EDIT = EDIT
     ELLIPSIS = ELLIPSIS
+    EMAIL = EMAIL
+    ENTROPY = ENTROPY
     EXCHANGE = EXCHANGE
     EXIT = EXIT
     EXPORT = EXPORT
+    FAUCET = FAUCET
     FILE = FILE
+    FINGERPRINT = FINGERPRINT
     FLIP_HORIZONTAL = FLIP_HORIZONTAL
     FLIP_VERTICAL = FLIP_VERTICAL
     GEAR = GEAR
+    GIFT = GIFT
     GLOBE = GLOBE
     GRAPH = GRAPH
     GRID = GRID
+    HASHES = HASHES
+    HAT_AND_GLASSES = HAT_AND_GLASSES
     HIDDEN = HIDDEN
     HOME = HOME
     INFO = INFO
     INFO_CIRCLE = INFO_CIRCLE
     INVOICE = INVOICE
+    IOU = IOU
     KEY = KEY
+    KEYBOARD = KEYBOARD
     LIGHTNING = LIGHTNING
     LIGHTNING_CIRCLE = LIGHTNING_CIRCLE
     LINK = LINK
@@ -219,10 +281,12 @@ class BTC_ICONS:
     MINER = MINER
     MINING = MINING
     MINING_DEVICE = MINING_DEVICE
+    MINING_POOL = MINING_POOL
     MINUS = MINUS
     MIXED = MIXED
     MNEMONIC = MNEMONIC
     MOON = MOON
+    NFC = NFC
     NO_DOLLARS = NO_DOLLARS
     NODE = NODE
     NODE_0_CONNECTIONS = NODE_0_CONNECTIONS
@@ -231,17 +295,22 @@ class BTC_ICONS:
     NODE_3_CONNECTIONS = NODE_3_CONNECTIONS
     NODE_HARDWARE = NODE_HARDWARE
     PANTHEON = PANTHEON
+    PAPER_BITCOIN = PAPER_BITCOIN
     PASSWORD = PASSWORD
     PHOTO = PHOTO
     PIE_CHART = PIE_CHART
     PLUS = PLUS
     PODCAST = PODCAST
     POINT_OF_SALE = POINT_OF_SALE
+    POWER = POWER
+    PRINTER = PRINTER
     PROXY = PROXY
     QR_CODE = QR_CODE
     QUESTION = QUESTION
     QUESTION_CIRCLE = QUESTION_CIRCLE
+    RECEIPT = RECEIPT
     RECEIVE = RECEIVE
+    RECEIVE_LIMIT = RECEIVE_LIMIT
     REFRESH = REFRESH
     RELAY = RELAY
     ROCKET = ROCKET
@@ -253,13 +322,16 @@ class BTC_ICONS:
     SD_CARD = SD_CARD
     SEARCH = SEARCH
     SEND = SEND
+    SEND_LIMIT = SEND_LIMIT
     SHARE = SHARE
     SHARED_WALLET = SHARED_WALLET
     SHIELD = SHIELD
     SIGN = SIGN
+    SIREN = SIREN
     SMARTCARD = SMARTCARD
     SNOWFLAKE = SNOWFLAKE
     SOFA = SOFA
+    SPLIT = SPLIT
     STAR = STAR
     SUN = SUN
     TAG = TAG
@@ -267,10 +339,16 @@ class BTC_ICONS:
     TRANSACTIONS = TRANSACTIONS
     TRANSFER = TRANSFER
     TRASH = TRASH
+    TREE_STRUCTURE = TREE_STRUCTURE
     TWO_KEYS = TWO_KEYS
     UNLOCK = UNLOCK
     UNMIXED = UNMIXED
     USB = USB
     VERIFY = VERIFY
     VISIBLE = VISIBLE
+    VOLUME_1 = VOLUME_1
+    VOLUME_2 = VOLUME_2
+    VOLUME_MAX = VOLUME_MAX
+    VOLUME_MUTE = VOLUME_MUTE
     WALLET = WALLET
+    WATER_BUCKET = WATER_BUCKET
